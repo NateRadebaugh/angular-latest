86a0a34a302e5a8f2fc12ffff63bce8b174f0aba
Nate Radebaugh <nradebaugh@bdo.com>
2022-11-21 18:00:37 -0600

    Standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
+  ],
 })
 export class AppComponent {
   title = 'angular-latest';
